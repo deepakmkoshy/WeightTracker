61878aba392d8d2195ca29b07aa495a834330e90
Deepak <deepakmkoshy@gmail.com>
2020-07-27 18:22:07 +0530

    Added input fields and adding inputs to a list, and printing it.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -16,87 +16,105 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   String _input;
-  List<String> _weight;
+  var _weight = <String>[];
 
-@override
-void initState(){
-  super.initState();
-  _input='';
-}
+  @override
+  void initState() {
+    super.initState();
+    _input = '';
+  }
 
-  void _updateList(String input){
-  setState(() {
-    _input=input;
-    _weight.add(input);
-  });
+  void _updateInput(String input) {
+    setState(() {
+      _input = input;
+    });
+  }
+
+  void _updateList() {
+    setState(() {
+      _weight.add(_input);
+    });
+  }
+
+  Widget _viewList() {
+    String tmp = '';
+    if (_weight.isEmpty)
+      return null;
+    else {
+      for (var text in _weight) {
+        tmp = tmp + ' ' + text;
+      }
+      return Text(
+        tmp,
+        style: Theme.of(context).textTheme.headline5,
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-         TextField(
-            style: Theme.of(context).textTheme.headline5,
-            decoration: InputDecoration(
-              labelText: "Enter your current weight",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.0)
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                  style: Theme.of(context).textTheme.headline5,
+                  decoration: InputDecoration(
+                    labelText: "Enter your current weight",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)),
+                  ),
+                  onSubmitted: _updateInput,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Ink(
+                  decoration: const ShapeDecoration(
+                    color: Colors.purpleAccent,
+                    shape: CircleBorder(),
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: _updateList,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Container(
+              child: InputDecorator(
+                child:
+                    Text(_input, style: Theme.of(context).textTheme.headline5),
+                decoration: InputDecoration(
+                  labelText: 'What you just inputted',
+                  labelStyle: Theme.of(context).textTheme.headline5,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
               ),
             ),
-            onSubmitted: _updateList,
-
-         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: Container(
-              child: InputDecorator(
-            child: Text(_input,
-            style: Theme.of(context).textTheme.headline5),
-            decoration: InputDecoration(
-    labelText: 'What you just inputted',
-    labelStyle: Theme.of(context).textTheme.headline5,
-    border: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(5.0)
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: _viewList(),
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.amber),
             ),
-
-
-          ),
-          ),
-        ),
-        ),
-
-
-
-      ],
+          )
+        ],
       ),
     );
-
-
-
-
-
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: DecoratedBox(
-          decoration: BoxDecoration(
-            color: Colors.orangeAccent,
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              "How does this box look",
-              style: TextStyle(fontSize: 30.0),
-            ),
-          ),
-        ),
-      //),
-    );
+    //);
   }
 }
